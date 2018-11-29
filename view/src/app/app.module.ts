e1e01675207581ba19bc7b5d34cb445795082e64
mauriciocordeiro <mauriciocordeiro@live.com>
2018-11-29 17:17:58 -0300

    reset

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		ContentComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserAnimationsModule,
+		BrowserModule,
+		AppRoutingModule,
+		MatDialogModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
